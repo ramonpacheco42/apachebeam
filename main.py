1aa900f29bd0177970d1ee7218db1b63cd4d27c8
ramonpacheco <ramonpacheco@pop-os.localdomain>
2022-07-16 11:44:23 -0300

    Inicio da aula 05 - Agrupando e retornando tupla

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.options.pipeline_options import PipelineOptions
@@ -6,53 +7,78 @@ pipeline_options = PipelineOptions(argv=None)
 pipeline = beam.Pipeline(options=pipeline_options)
 
 colunas_dengue = [
-                'id',
-                'data_iniSE',
-                'casos',
-                'ibge_code',
-                'cidade',
-                'uf',
-                'cep',
-                'latitude',
-                'longitude']
+        'id',
+        'data_iniSE',
+        'casos',
+        'ibge_code',
+        'cidade',
+        'uf',
+        'cep',
+        'latitude',
+        'longitude']
 
-def list_to_dict(element, columns):
+def lista_para_dicionario(elemento, colunas):
     """
     Recebe 2 listas
-    Retorna 1 Dicionário.
+    Retorna 1 dicionário
     """
-    return dict(zip(columns,element))
+    return dict(zip(colunas, elemento))
 
-def text_to_list(element, delimitador='|'):
+def texto_para_lista(elemento, delimitador='|'):
     """
-    Recebe um texto com delimitador
-    Retorna uma lista de elementos pelo delimitador.
+    Recebe um texto e um delimitador
+    Retorna uma lista de elementos pelo delimitador
     """
-    return element.split(delimitador)
+    return elemento.split(delimitador)
 
-def do_date(data):
+def trata_datas(elemento):
     """
-    Recebe um dicionário e cria uma nova coluna com ANO-MES
+    Recebe um dicionário e cria um novo campo com ANO-MES
     Retorna o mesmo dicionário com o novo campo
     """
-    data['ano_mes'] = '-'.join(data['data_iniSE'].split('-')[:2])
-    return data
+    elemento['ano_mes'] = '-'.join(elemento['data_iniSE'].split('-')[:2])
+    return elemento
 
-def key_uf(element):
-    
-    chave = element['uf']
-    return (chave, element)
+def chave_uf(elemento):
+    """
+    Receber um dicionário
+    Retornar uma tupla com o estado(UF) e o elemento (UF, dicionário)
+    """
+    chave = elemento['uf']
+    return (chave, elemento)
 
-dengue = (
+def casos_dengue(elemento):
+    """
+    Recebe uma tupla ('RS', [{}, {}])
+    Retornar uma tupla ('RS-2014-12', 8.0)
+    """
+    uf, registros = elemento
+    for registro in registros:
+        if bool(re.search(r'\d', registro['casos'])):
+            yield (f"{uf}-{registro['ano_mes']}", float(registro['casos']))
+        else:
+            yield (f"{uf}-{registro['ano_mes']}", 0.0)
+
+# dengue = (
+#     pipeline
+#     | "Leitura do dataset de dengue" >>
+#     ReadFromText('./data/casos_dengue.txt', skip_header_lines=1)
+#     | "De texto para lista" >> beam.Map(texto_para_lista)
+#     | "De lista para dicionário" >> beam.Map(lista_para_dicionario, colunas_dengue)
+#     | "Criar campo ano_mes" >> beam.Map(trata_datas)
+#     | "Criar chave pelo estado" >> beam.Map(chave_uf)
+#     | "Agrupar pelo estado" >> beam.GroupByKey()
+#     | "Descompactar casos de dengue" >> beam.FlatMap(casos_dengue)
+#     | "Soma dos casos pela chave" >> beam.CombinePerKey(sum)
+#     | "Mostrar resultados" >> beam.Map(print)
+# )
+
+chuvas = (
     pipeline
     | "Leitura do dataset de dengue" >>
-        ReadFromText('./data/casos_dengue.txt', skip_header_lines=1)
-    | "De texto para lista" >> beam.Map(text_to_list)
-    | "De Lista para Dicionário" >> beam.Map(list_to_dict, colunas_dengue)
-    | "Criar campo ano_mes" >> beam.Map(do_date)
-    | "Criar chave pelo estado" >> beam.Map(key_uf)
-    | "Agrupar pelo estado" >> beam.GroupBy()
-    | "Mostar Resultados" >> beam.Map(print)
+        ReadFromText('./data/chuvas.csv', skip_header_lines=1)
+    | "De texto para lista (chuvas)" >> beam.Map(texto_para_lista, delimitador=',')
+    | "Mostrar resultados de chuvas" >> beam.Map(print)
 )
 
 pipeline.run()
